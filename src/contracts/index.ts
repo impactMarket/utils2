5a56809676d1c7141466daff2d0cec47d7a1558b
Bernardo Vieira <bernardo@impactmarket.com>
2021-11-25 19:45:19 +0000

    update contract addresses

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -13,11 +13,11 @@ export const ContractAddresses = new Map<
         44787,
         {
             cUSD: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
-            DonationMiner: '0x6dA6ce3CFAaDBA1A08ebCF01AF8F70956437962d',
-            CommunityAdmin: '0xB42D6F6B71CDE74e431565c9236DF7b9bDCFD7E5',
-            PACTDelegate: '0x10e633e7FFeFb800067fDE3960d1Db3cD00D0EBE',
-            PACTDelegator: '0xE2cc4203117a2f753e9a86630B51e1128528ECB5',
-            PACTToken: '0x3Fd6010207c9BF4BbA84F82146a7bEEd4cf2DD2d'
+            DonationMiner: '0x2DdFCB97246Eb4767a92E5d31B9B4ef0C6e8f296',
+            CommunityAdmin: '0xc05E9866278d9F9038603916d6acfA1606cF486F',
+            PACTDelegate: '0x63845669d3Ad71043aB7b3190111379c1Ece5c14',
+            PACTDelegator: '0xFb9B517bA9c771E1009596c4AD60C626CE6990Ca',
+            PACTToken: '0x4A95f3b625e59ba1F1b2dbBb84B94b0eE65A35DE'
         }
     ]
 ]);
