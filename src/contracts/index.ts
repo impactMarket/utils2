bde9574c2c43fdb2e60134a9ea93e956a61d6312
Bernardo Vieira <bernardo@impactmarket.com>
2021-11-25 17:26:52 +0000

    update addresses + abi

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,22 +1,23 @@
-export const cusdContractAddress = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
-export const pactContractAddress = '0xAb0EE41B8d3540d6145E2Eb58B84a55142aac345';
-
 export const ContractAddresses = new Map<
     number,
     {
+        cUSD: string;
         DonationMiner: string;
         CommunityAdmin: string;
         PACTDelegate: string;
         PACTDelegator: string;
+        PACTToken: string;
     }
 >([
     [
         44787,
         {
-            DonationMiner: '0xF50A9aa0013AeF09C640547823542F1c7Bc2DA03',
-            CommunityAdmin: '0x66d377462fa7B240dEA63BbDD299C44Df737BC9C',
-            PACTDelegate: '0x55fbea12B9d2E50c820cd6983398C3b9e88Cdc4c',
-            PACTDelegator: '0x6b69e79B899e634092384Be1d09a41B6f4886a6e'
+            cUSD: '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1',
+            DonationMiner: '0x6dA6ce3CFAaDBA1A08ebCF01AF8F70956437962d',
+            CommunityAdmin: '0xB42D6F6B71CDE74e431565c9236DF7b9bDCFD7E5',
+            PACTDelegate: '0x10e633e7FFeFb800067fDE3960d1Db3cD00D0EBE',
+            PACTDelegator: '0xE2cc4203117a2f753e9a86630B51e1128528ECB5',
+            PACTToken: '0x3Fd6010207c9BF4BbA84F82146a7bEEd4cf2DD2d'
         }
     ]
 ]);
