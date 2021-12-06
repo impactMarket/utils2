2aa694ee6b4814380d874e54bc192d812e9eb5cc
Bernardo Vieira <bernardo@impactmarket.com>
2021-12-06 12:21:02 +0000

    estimateBlockTime

---
+++ src/helpers/estimateBlockTime.ts
@@ -0,0 +1,12 @@
+/**
+ * Estimate remaining time until the end block, in seconds.
+ * If the end block was in the past, add another epoch.
+ */
+export const estimateBlockTime = (currentBlock: number, endBlock: number) => {
+    const isFuture = currentBlock > endBlock;
+    const blocksPerDay = 12 * 60 * 24; // ~ amount of blocks in a day
+    const blockTime =
+        (endBlock - currentBlock + (isFuture ? blocksPerDay : 0)) * 5000;
+
+    return new Date(new Date().getTime() + blockTime);
+};
