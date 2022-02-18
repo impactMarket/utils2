0aef84c5a9687e600f49fb7ca7f6663462be38af
Bernardo Vieira <bernardo@impactmarket.com>
2022-02-18 23:48:45 +0000

    beneficiary live refresh

--- src/__tests__/estimateBlockTime.test.ts
+++ src/__tests__/estimateBlockTime.test.ts
@@ -11,6 +11,7 @@ describe('#estimateBlockTime()', () => {
         const result = estimateBlockTime(87000, 87000 + blocksPerDay);
         const expectedDate = new Date();
         expectedDate.setDate(expectedDate.getDate() + 1);
+        expectedDate.setTime(expectedDate.getTime() + 2000); // 2s overflow
         expect(testableDate(result)).to.equal(testableDate(expectedDate));
     });
     it('future (half day)', () => {
--- src/useBeneficiary.tsx
+++ src/useBeneficiary.tsx
@@ -6,23 +6,27 @@ import { toNumber } from './toNumber';
 import { ImpactProviderContext } from './ImpactProvider';
 import { updateCUSDBalance } from './useCUSDBalance';
 
+const refreshIntervalInMs = 300000;
+
 /**
  * useBeneficiary hook
  * @dev Hook for beneficiary UI
  */
 export const useBeneficiary = (communityAddress: string) => {
     const [isReady, setIsReady] = useState(false);
+    const [isClaimable, setIsClaimable] = useState(false);
     const [beneficiary, setBeneficiary] = useState<{
         claimedAmount: number;
     }>({
         claimedAmount: 0
     });
-    const [claimCooldown, setClaimCooldown] = useState(new Date(0));
+    const [claimCooldown, setClaimCooldown] = useState(0);
     const [contract, setContract] = useState<Contract | null>(null);
     const { provider, address, signer } = React.useContext(
         ImpactProviderContext
     );
     let refreshInterval: NodeJS.Timeout;
+    let timeoutInterval: NodeJS.Timeout;
 
     const updateClaimData = async (_contract: Contract) => {
         if (!_contract || !address) {
@@ -61,9 +65,42 @@ export const useBeneficiary = (communityAddress: string) => {
 
         updateClaimData(contract);
         updateCUSDBalance(provider, address);
+        const _cooldown = await contract.claimCooldown(address);
+        let _currentBlockNumber = await provider.getBlockNumber();
+        const estimate = async () => {
+            _currentBlockNumber = await provider.getBlockNumber();
+            return estimateBlockTime(_currentBlockNumber, _cooldown.toNumber());
+        };
+        setClaimCooldown((await estimate()).getTime());
+        setIsClaimable(false);
 
         return response;
     };
+
+    // two intervals are needed
+    // one to refresh the end date (updating according to remaining blocks)
+    // and the other to countdown until end date and refresh the claimable status
+
+    useEffect(() => {
+        if (claimCooldown !== 0 && timeoutInterval === undefined) {
+            const end = claimCooldown;
+            const now = new Date().getTime();
+            if (end > now && end - now < refreshIntervalInMs) {
+                timeoutInterval = setTimeout(() => {
+                    setIsClaimable(true);
+                    if (timeoutInterval) {
+                        clearTimeout(timeoutInterval);
+                    }
+                    if (refreshInterval) {
+                        clearInterval(refreshInterval);
+                    }
+                }, end - now - 1000);
+            }
+        }
+        return () => {
+            clearTimeout(timeoutInterval);
+        };
+    }, [claimCooldown]);
 
     useEffect(() => {
         // Refresh beneficiary time for next claim
@@ -72,17 +109,32 @@ export const useBeneficiary = (communityAddress: string) => {
             _contract: Contract
         ) => {
             const _cooldown = await _contract.claimCooldown(_address);
-            const _currentBlockNumber = await provider.getBlockNumber();
+            let _currentBlockNumber = await provider.getBlockNumber();
             if (_cooldown.toNumber() > _currentBlockNumber) {
-                const estimate = async () =>
-                    estimateBlockTime(
-                        _currentBlockNumber,
-                        _cooldown.toNumber()
-                    );
-                refreshInterval = setInterval(async () => {
-                    setClaimCooldown(await estimate());
-                }, 10000);
-                setClaimCooldown(await estimate());
+                const estimate = async () => {
+                    _currentBlockNumber = await provider.getBlockNumber();
+                    return _cooldown.toNumber() > _currentBlockNumber
+                        ? estimateBlockTime(
+                              _currentBlockNumber,
+                              _cooldown.toNumber()
+                          )
+                        : new Date(0);
+                };
+                if (refreshInterval) {
+                    clearInterval(refreshInterval);
+                }
+                refreshInterval = setInterval(() => {
+                    estimate().then((d) => {
+                        if (d.getTime() === 0 && refreshInterval) {
+                            clearInterval(refreshInterval);
+                        } else {
+                            setClaimCooldown(d.getTime());
+                        }
+                    });
+                }, refreshIntervalInMs);
+                setClaimCooldown((await estimate()).getTime());
+            } else {
+                setIsClaimable(true);
             }
         };
         if (address && provider) {
@@ -97,5 +149,5 @@ export const useBeneficiary = (communityAddress: string) => {
         };
     }, []);
 
-    return { claim, beneficiary, claimCooldown, isReady };
+    return { claim, beneficiary, claimCooldown, isClaimable, isReady };
 };
