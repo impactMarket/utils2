d2645be0c1106af779b757cf6b3276d01fc27018
Bernardo Vieira <bernardo@impactmarket.com>
2022-03-10 21:53:20 +0000

    increase gas limit v1 support

--- src/hooks/useDonationMiner.tsx
+++ src/hooks/useDonationMiner.tsx
@@ -63,12 +63,25 @@ export const useDonationMiner = (): DonationMinerType => {
     const donateToTreasury = async (value: string | number) => {
         try {
             if (!signer) {
-                return;
+                return 0;
             }
             const amount = toToken(value, { EXPONENTIAL_AT: 29 });
             const { donationMiner } = await getContracts(provider);
-            const tx = await donationMiner.connect(signer).donate(amount);
-            const response = await tx.wait();
+            const tx = await donationMiner.populateTransaction.donate(amount);
+            const gasLimit = await signer.estimateGas(tx);
+            const gasPrice = await signer.getGasPrice();
+            
+            // Gas estimation doesn't currently work properly
+            // The gas limit must be padded to increase tx success rate
+            // TODO: Investigate more efficient ways to handle this case
+            const adjustedGasLimit = gasLimit.mul(2);
+
+            const txResponse = await signer.sendTransaction({
+                ...tx,
+                gasLimit: adjustedGasLimit,
+                gasPrice,
+            })
+            const response = await txResponse.wait();
 
             await updateCUSDBalance();
             updateRewards();
@@ -77,6 +90,7 @@ export const useDonationMiner = (): DonationMinerType => {
             return response;
         } catch (error) {
             console.log('Error in donateToTreasury function: \n', error);
+            return 0;
         }
     };
 
