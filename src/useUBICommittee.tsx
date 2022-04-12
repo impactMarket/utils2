69774e567f41ca7fe0456e52742d5d06a63423cd
Bernardo Vieira <bernardo@impactmarket.com>
2022-04-12 20:50:14 +0100

    base UBICommitee proposals types

---
+++ src/useUBICommittee.tsx
@@ -0,0 +1,249 @@
+import { BigNumber } from 'bignumber.js';
+import { ImpactProviderContext } from './ImpactProvider';
+import { Interface, defaultAbiCoder } from '@ethersproject/abi';
+import { executeTransaction } from './executeTransaction';
+import { getContracts } from './contracts';
+import React from 'react';
+import UBICommitteeABI from './abi/UBICommittee.json';
+
+type CommunityArgs = {
+    baseInterval: string | BigNumber;
+    claimAmount: string | BigNumber;
+    decreaseStep: string | BigNumber;
+    ambassador: string;
+    managers: string[];
+    incrementInterval: string | BigNumber;
+    maxClaim: string | BigNumber;
+    maxTranche: string | BigNumber;
+    minTranche: string | BigNumber;
+    proposalTitle: string;
+    proposalDescription: string;
+};
+type CommunityRemoveArgs = {
+    communityAddress: string;
+    proposalTitle: string;
+    proposalDescription: string;
+};
+type CommunityUpdateParamsArgs = {
+    communityAddress: string;
+    minTranche: string | BigNumber;
+    maxTranche: string | BigNumber;
+    proposalTitle: string;
+    proposalDescription: string;
+};
+type CommunityUpdateBeneficiaryParamsArgs = {
+    communityAddress: string;
+    baseInterval: string | BigNumber;
+    incrementInterval: string | BigNumber;
+    claimAmount: string | BigNumber;
+    maxClaim: string | BigNumber;
+    decreaseStep: string | BigNumber;
+    proposalTitle: string;
+    proposalDescription: string;
+};
+
+export const useUBICommittee = () => {
+    const { connection, address, provider } = React.useContext(ImpactProviderContext);
+
+    /**
+     * @dev Generates proposal to create new community
+     * @param {CommunityArgs} community community parameters
+     * @returns {Promise<number>} proposal id
+     */
+    const addCommunity = async (community: CommunityArgs) => {
+        if (!connection || !address) {
+            return;
+        }
+        const { cusd, ubiCommittee, addresses } = await getContracts(provider);
+        const {
+            baseInterval,
+            claimAmount,
+            decreaseStep,
+            managers,
+            ambassador,
+            incrementInterval,
+            maxClaim,
+            maxTranche,
+            minTranche,
+            proposalTitle,
+            proposalDescription
+        } = community;
+        const targets = [addresses.communityAdmin];
+        const values = [0];
+        const signatures = ['addCommunity(address[],address,uint256,uint256,uint256,uint256,uint256,uint256,uint256)'];
+
+        const calldatas = [
+            defaultAbiCoder.encode(
+                ['address[]', 'address', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
+                [
+                    managers,
+                    ambassador,
+                    claimAmount,
+                    maxClaim,
+                    decreaseStep,
+                    baseInterval,
+                    incrementInterval,
+                    minTranche,
+                    maxTranche
+                ]
+            )
+        ];
+
+        const tx = await ubiCommittee.populateTransaction.propose(
+            targets,
+            values,
+            signatures,
+            calldatas,
+            JSON.stringify({
+                description: proposalDescription,
+                title: proposalTitle
+            })
+        );
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+        const ifaceDAO = new Interface(UBICommitteeABI);
+        
+        return parseInt(ifaceDAO.parseLog(response.logs[0]).args![0].toString(), 10);
+    };
+
+    /**
+     * @dev Generates proposal to remove a community
+     * @param {CommunityRemoveArgs} community community parameters
+     * @returns {Promise<number>} proposal id
+     */
+    const removeCommunity = async (community: CommunityRemoveArgs) => {
+        if (!connection || !address) {
+            return;
+        }
+        const { cusd, ubiCommittee, addresses } = await getContracts(provider);
+        const {
+            communityAddress,
+            proposalTitle,
+            proposalDescription
+        } = community;
+        const targets = [addresses.communityAdmin];
+        const values = [0];
+        const signatures = ['removeCommunity(address)'];
+
+        const calldatas = [
+            defaultAbiCoder.encode(
+                ['address'],
+                [communityAddress]
+            )
+        ];
+
+        const tx = await ubiCommittee.populateTransaction.propose(
+            targets,
+            values,
+            signatures,
+            calldatas,
+            JSON.stringify({
+                description: proposalDescription,
+                title: proposalTitle
+            })
+        );
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+        const ifaceDAO = new Interface(UBICommitteeABI);
+        
+        return parseInt(ifaceDAO.parseLog(response.logs[0]).args![0].toString(), 10);
+    };
+
+    /**
+     * @dev Generates proposal to change community params
+     * @param {CommunityUpdateParamsArgs} community community parameters
+     * @returns {Promise<number>} proposal id
+     */
+    const updateCommunityParams = async (community: CommunityUpdateParamsArgs) => {
+        if (!connection || !address) {
+            return;
+        }
+        const { cusd, ubiCommittee, addresses } = await getContracts(provider);
+        const {
+            communityAddress,
+            minTranche,
+            maxTranche,
+            proposalTitle,
+            proposalDescription
+        } = community;
+        const targets = [addresses.communityAdmin];
+        const values = [0];
+        const signatures = ['updateCommunityParams(address,uint256,uint256)'];
+
+        const calldatas = [
+            defaultAbiCoder.encode(
+                ['address', 'uint256', 'uint256'],
+                [communityAddress, minTranche, maxTranche]
+            )
+        ];
+
+        const tx = await ubiCommittee.populateTransaction.propose(
+            targets,
+            values,
+            signatures,
+            calldatas,
+            JSON.stringify({
+                description: proposalDescription,
+                title: proposalTitle
+            })
+        );
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+        const ifaceDAO = new Interface(UBICommitteeABI);
+        
+        return parseInt(ifaceDAO.parseLog(response.logs[0]).args![0].toString(), 10);
+    };
+
+    /**
+     * @dev Generates proposal to change community beneficiary params
+     * @param {CommunityUpdateBeneficiaryParamsArgs} community community parameters
+     * @returns {Promise<number>} proposal id
+     */
+    const updateBeneficiaryParams = async (community: CommunityUpdateBeneficiaryParamsArgs) => {
+        if (!connection || !address) {
+            return;
+        }
+        const { cusd, ubiCommittee, addresses } = await getContracts(provider);
+        const {
+            communityAddress,
+            baseInterval,
+            claimAmount,
+            decreaseStep,
+            incrementInterval,
+            maxClaim,
+            proposalTitle,
+            proposalDescription
+        } = community;
+        const targets = [addresses.communityAdmin];
+        const values = [0];
+        const signatures = ['updateBeneficiaryParams(address,uint256,uint256,uint256,uint256,uint256)'];
+
+        const calldatas = [
+            defaultAbiCoder.encode(
+                ['address', 'uint256', 'uint256', 'uint256', 'uint256', 'uint256'],
+                [
+                    communityAddress,
+                    claimAmount,
+                    maxClaim,
+                    decreaseStep,
+                    baseInterval,
+                    incrementInterval,
+                ]
+            )
+        ];
+
+        const tx = await ubiCommittee.populateTransaction.propose(
+            targets,
+            values,
+            signatures,
+            calldatas,
+            JSON.stringify({
+                description: proposalDescription,
+                title: proposalTitle
+            })
+        );
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+        const ifaceDAO = new Interface(UBICommitteeABI);
+        
+        return parseInt(ifaceDAO.parseLog(response.logs[0]).args![0].toString(), 10);
+    };
+
+    return { addCommunity, removeCommunity, updateBeneficiaryParams, updateCommunityParams };
+};
