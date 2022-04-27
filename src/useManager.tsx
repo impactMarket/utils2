91104b7a16a6c89e462ff70426477220fb129496
Bernardo Vieira <bernardo@impactmarket.com>
2022-04-27 22:17:18 +0100

    use manager hook new methods

--- src/useManager.tsx
+++ src/useManager.tsx
@@ -3,6 +3,7 @@ import { communityContract } from './community';
 import { executeTransaction } from './executeTransaction';
 import { getContracts } from './contracts';
 import React from 'react';
+import type { CeloTxReceipt } from '@celo/connect';
 
 export const useManager = (communityAddress: string) => {
     const { connection, address, provider } = React.useContext(ImpactProviderContext);
@@ -10,7 +11,7 @@ export const useManager = (communityAddress: string) => {
     /**
      * Add beneficiary to community
      * @param {string} beneficiaryAddress Beneficiary address to be added
-     * @returns {ethers.ContractReceipt} transaction response object
+     * @returns {Promise<CeloTxReceipt>} transaction response object
      * @throws {Error} "Community: NOT_MANAGER"
      * @example
      * ```typescript
@@ -18,9 +19,9 @@ export const useManager = (communityAddress: string) => {
      * const tx = await addBeneficiary('beneficiary-address');
      * ```
      */
-    const addBeneficiary = async (beneficiaryAddress: string) => {
+    const addBeneficiary = async (beneficiaryAddress: string): Promise<CeloTxReceipt> => {
         if (!connection || !address) {
-            return;
+            throw new Error('No connection');
         }
         const { cusd } = await getContracts(provider);
         const tx = await communityContract(communityAddress).populateTransaction.addBeneficiary(beneficiaryAddress);
@@ -32,13 +33,18 @@ export const useManager = (communityAddress: string) => {
     /**
      * Remove beneficiary from community
      * @param {string} beneficiaryAddress Beneficiary address to be removed
-     * @returns {ethers.ContractReceipt} transaction response object
+     * @returns {Promise<CeloTxReceipt>} transaction response object
      * @throws {Error} "Community: NOT_MANAGER"
      * @throws {Error} "Community::removeBeneficiary: NOT_YET"
+     * @example
+     * ```typescript
+     * const { removeBeneficiary } = useManager('community-address');
+     * const tx = await removeBeneficiary('beneficiary-address');
+     * ```
      */
-    const removeBeneficiary = async (beneficiaryAddress: string) => {
+    const removeBeneficiary = async (beneficiaryAddress: string): Promise<CeloTxReceipt> => {
         if (!connection || !address) {
-            return;
+            throw new Error('No connection');
         }
         const { cusd } = await getContracts(provider);
         const tx = await communityContract(communityAddress).populateTransaction.removeBeneficiary(beneficiaryAddress);
@@ -47,5 +53,80 @@ export const useManager = (communityAddress: string) => {
         return response;
     };
 
-    return { addBeneficiary, removeBeneficiary };
+    /**
+     * Lock beneficiary in community
+     * @param {string} beneficiaryAddress Beneficiary address to be locked
+     * @returns {Promise<CeloTxReceipt>} transaction response object
+     * @throws {Error} "Community: NOT_MANAGER"
+     * @throws {Error} "Community::lockBeneficiary: NOT_YET"
+     * @example
+     * ```typescript
+     * const { lockBeneficiary } = useManager('community-address');
+     * const tx = await lockBeneficiary('beneficiary-address');
+     * ```
+     */
+    const lockBeneficiary = async (beneficiaryAddress: string): Promise<CeloTxReceipt> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const { cusd } = await getContracts(provider);
+        const tx = await communityContract(communityAddress).populateTransaction.lockBeneficiary(beneficiaryAddress);
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+
+        return response;
+    };
+
+    /**
+     * Unlock beneficiary in community
+     * @param {string} beneficiaryAddress Beneficiary address to be unlocked
+     * @returns {Promise<CeloTxReceipt>} transaction response object
+     * @throws {Error} "Community: NOT_MANAGER"
+     * @throws {Error} "Community::unlockBeneficiary: NOT_YET"
+     * @example
+     * ```typescript
+     * const { unlockBeneficiary } = useManager('community-address');
+     * const tx = await unlockBeneficiary('beneficiary-address');
+     * ```
+     */
+    const unlockBeneficiary = async (beneficiaryAddress: string): Promise<CeloTxReceipt> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const { cusd } = await getContracts(provider);
+        const tx = await communityContract(communityAddress).populateTransaction.unlockBeneficiary(beneficiaryAddress);
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+
+        return response;
+    };
+
+    /**
+     * Allow manager to request funds from the DAO treasury
+     * @returns {Promise<CeloTxReceipt>} transaction response object
+     * @throws {Error} "CommunityAdmin::fundCommunity: Not enough funds"
+     * @throws {Error} "ERC20: transfer amount exceeds balance"
+     * @throws {Error} "Community: NOT_MANAGER"
+     * @example
+     * ```typescript
+     * const { requestFunds } = useManager('community-address');
+     * const tx = await requestFunds();
+     * ```
+     */
+    const requestFunds = async (): Promise<CeloTxReceipt> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const { cusd } = await getContracts(provider);
+        const tx = await communityContract(communityAddress).populateTransaction.requestFunds();
+        const response = await executeTransaction(connection, address, cusd.address, tx);
+
+        return response;
+    };
+
+    return {
+        addBeneficiary,
+        lockBeneficiary,
+        removeBeneficiary,
+        requestFunds,
+        unlockBeneficiary
+    };
 };
