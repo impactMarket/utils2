0feeb87107505140c7a5bc7e7c41b96bc0946df8
Bernardo Vieira <bernardo@impactmarket.com>
2022-05-31 13:17:27 +0100

    cleanup metrics example (#122)

--- example/src/components/PACTMetrics/index.tsx
+++ example/src/components/PACTMetrics/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import WalletConnection from '../WalletConnection';
 import { getPACTTradingMetrics, getPACTTVL, getUBILiquidity } from '@impact-market/utils/pact';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { CeloMainnet } from '@celo-tools/use-contractkit';
@@ -13,7 +12,15 @@ const PACTMetrics = () => {
         transfers: number;
         tvl: number;
         ubiLiquidity: number;
-    }>({ priceUSD: '', dailyVolumeUSD: '', totalLiquidityUSD: '', tokenHolders: 0, transfers: 0, tvl: 0, ubiLiquidity: 0 });
+    }>({
+        priceUSD: '',
+        dailyVolumeUSD: '',
+        totalLiquidityUSD: '',
+        tokenHolders: 0,
+        transfers: 0,
+        tvl: 0,
+        ubiLiquidity: 0
+    });
 
     useEffect(() => {
         const loadPactPriceVolumeLiquidity = async () => {
@@ -21,39 +28,36 @@ const PACTMetrics = () => {
             const r = await getPACTTradingMetrics(provider);
             const tvl = await getPACTTVL(provider);
             const ubiLiquidity = await getUBILiquidity(provider);
-            console.log({ ...r, tvl, ubiLiquidity })
-            setPactTradingMetrics({ ...r, tvl, ubiLiquidity });
-        }
+            setPactTradingMetrics({ ...r, tvl: parseInt(tvl, 10), ubiLiquidity });
+        };
         loadPactPriceVolumeLiquidity();
     }, []);
 
     return (
-        <WalletConnection title="PACT Metrics">
-            <ul>
-                <li style={{ marginTop: 16 }}>
-                    <div>priceUSD ${pactTradingMetrics.priceUSD}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>dailyVolumeUSD {pactTradingMetrics.dailyVolumeUSD}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>totalLiquidityUSD {pactTradingMetrics.totalLiquidityUSD}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>tokenHolders {pactTradingMetrics.tokenHolders}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>transfers {pactTradingMetrics.transfers}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>tvl {pactTradingMetrics.tvl}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>ubiLiquidity {pactTradingMetrics.ubiLiquidity}</div>
-                </li>
-            </ul>
-        </WalletConnection>
+        <ul>
+            <li style={{ marginTop: 16 }}>
+                <div>priceUSD ${pactTradingMetrics.priceUSD}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>dailyVolumeUSD {pactTradingMetrics.dailyVolumeUSD}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>totalLiquidityUSD {pactTradingMetrics.totalLiquidityUSD}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>tokenHolders {pactTradingMetrics.tokenHolders}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>transfers {pactTradingMetrics.transfers}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>tvl {pactTradingMetrics.tvl}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>ubiLiquidity {pactTradingMetrics.ubiLiquidity}</div>
+            </li>
+        </ul>
     );
-}
+};
 
 export default PACTMetrics;
