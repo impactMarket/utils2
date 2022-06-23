dabc8ff1281a1a35fa321a474f6b04620b3e2b44
Bernardo Vieira <bernardo@impactmarket.com>
2022-06-23 11:18:04 +0200

    loading staking no wallet (#132)

--- example/src/components/Staking/index.tsx
+++ example/src/components/Staking/index.tsx
@@ -11,40 +11,38 @@ const Staking = () => {
     const balancePACT = usePACTBalance();
 
     return (
-        <WalletConnection title="Staking">
-            <ul>
-                <li style={{ marginTop: 16 }}>
-                    <div>pact: {balancePACT}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>stakedAmount: {staking.stakedAmount} PACT</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>userAPR: {staking.userAPR}%</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>generalAPR: {staking.generalAPR}%</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>claimableUnstaked: {staking.claimableUnstaked} PACT</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>estimateClaimableRewardByStaking: {staking.estimateClaimableRewardByStaking} PACT</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>allocated: {staking.allocated} PACT</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>unstakeCooldown: {staking.unstakeCooldown}</div>
-                </li>
-                <li style={{ marginTop: 16 }}>
-                    <div>initialised: {staking.initialised.toString()}</div>
-                </li>
-            </ul>
-            <ApproveStake />
-            <Unstake />
-            <ClaimUnstaked />
-        </WalletConnection>
+        <ul>
+            <li style={{ marginTop: 16 }}>
+                <div>pact: {balancePACT}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>stakedAmount: {staking.stakedAmount} PACT</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>userAPR: {staking.userAPR}%</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>generalAPR: {staking.generalAPR}%</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>claimableUnstaked: {staking.claimableUnstaked} PACT</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>estimateClaimableRewardByStaking: {staking.estimateClaimableRewardByStaking} PACT</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>allocated: {staking.allocated} PACT</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>unstakeCooldown: {staking.unstakeCooldown}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>totalStaked: {staking.totalStaked}</div>
+            </li>
+            <li style={{ marginTop: 16 }}>
+                <div>initialised: {staking.initialised.toString()}</div>
+            </li>
+        </ul>
     );
 };
 
