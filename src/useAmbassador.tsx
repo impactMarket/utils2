7341fc01e6340175f154041cbdd2dfc8732cc2a2
Paulo Sousa <paulo.86.sousa@gmail.com>
2022-08-29 13:22:02 +0100

    Add methods to update and get max beneficiaries number and lock/unlock community

--- src/useAmbassador.tsx
+++ src/useAmbassador.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import type { CeloTxReceipt } from '@celo/connect';
 
 export const useAmbassador = () => {
-    const { connection, address } = React.useContext(ImpactProviderContext);
+    const { connection, address, provider } = React.useContext(ImpactProviderContext);
     const executeTransaction = internalUseTransaction();
 
     /**
@@ -79,5 +79,65 @@ export const useAmbassador = () => {
         return response;
     };
 
-    return { addManager, lockCommunity, removeManager, unlockCommunity };
+    /**
+     * Update max number of beneficiaries in the community
+     * @param {string} communityAddress Community address
+     * @param {string} maxBeneficiaries Maximum number of beneficiaries in the community
+     * @returns {Promise<CeloTxReceipt>} transaction response object
+     * @throws {Error} "No connection"
+     */
+    const updateMaxBeneficiaries = async (
+        communityAddress: string,
+        maxBeneficiaries: number
+    ): Promise<CeloTxReceipt> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const tx = await communityContract(communityAddress).populateTransaction.updateMaxBeneficiaries(
+            maxBeneficiaries
+        );
+        const response = await executeTransaction(tx);
+
+        return response;
+    };
+
+    /**
+     * Get max number of beneficiaries in the community
+     * @param {string} communityAddress Community address
+     * @returns {Promise<Number>} Maximum number of beneficiaries
+     * @throws {Error} "No connection"
+     */
+    const getMaxBeneficiaries = async (communityAddress: string): Promise<number> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const max = await communityContract(communityAddress, provider).maxBeneficiaries();
+
+        return max.toNumber();
+    };
+
+    /**
+     * Get if the community is locked
+     * @param {string} communityAddress Community address
+     * @returns {Promise<boolean>} Is the community locked
+     * @throws {Error} "No connection"
+     */
+    const isCommunityLocked = async (communityAddress: string): Promise<boolean> => {
+        if (!connection || !address) {
+            throw new Error('No connection');
+        }
+        const locked = await communityContract(communityAddress, provider).locked();
+
+        return locked;
+    };
+
+    return {
+        addManager,
+        getMaxBeneficiaries,
+        isCommunityLocked,
+        lockCommunity,
+        removeManager,
+        unlockCommunity,
+        updateMaxBeneficiaries
+    };
 };
