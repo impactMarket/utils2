9a6d4391a04378f0b51d01489c8aa00f560612a0
Bernardo Vieira <bernardo@impactmarket.com>
2022-08-30 17:25:15 +0200

    fix signatures (#160)

--- src/useSignatures.tsx
+++ src/useSignatures.tsx
@@ -1,4 +1,5 @@
 import { ImpactProviderContext } from './ImpactProvider';
+import { hashMessage } from '@ethersproject/hash';
 import { hexlify } from '@ethersproject/bytes';
 import { toUtf8Bytes } from '@ethersproject/strings';
 import React from 'react';
@@ -13,14 +14,25 @@ export const useSignatures = () => {
      * @returns {Promise<string>} signature
      */
     const signMessage = (message: string): Promise<string> => {
+        const connectionProvider = connection.web3.currentProvider as unknown as {
+            existingProvider: { isDesktop: boolean; _metamask?: unknown };
+        };
+
+        // it is not yet clear why, but metamask is not working with hexlify/toUtf8Bytes
+
+        if (connectionProvider.existingProvider.isDesktop || connectionProvider.existingProvider._metamask) {
+            return connection.sign(hashMessage(message), address!);
+        }
+
         return connection.sign(hexlify(toUtf8Bytes(message)), address!);
     };
 
-    // TODO:
-    /* const sign = (message: string) => {
-        return connection.signTypedData(getAddress(address!), {
+    // TODO: needs further testing
+    // works on mobile, but not on desktop. Seems to be an issue with react-celo and metamask
+    const signTyped = () => {
+        return connection.signTypedData(address!, {
             domain: {
-                chainId: 1,
+                chainId: 44787,
                 name: 'name',
                 verifyingContract: '0xB6Fa4E9B48F6fAcd8746573d8e151175c40121C7',
                 version: '1'
@@ -39,7 +51,7 @@ export const useSignatures = () => {
                 Test: [{ name: 'Request', type: 'string' }]
             }
         });
-    }; */
+    };
 
-    return { signMessage };
+    return { signMessage, signTyped };
 };
