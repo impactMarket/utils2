bb87d2e6c92058dbf3db21e3f2e725b0b32a1a93
Bernardo Vieira <bernardo@impactmarket.com>
2023-04-06 09:22:57 -0300

    add signTypedData to useSignatures (#217)

--- example-web/pages/components/Signatures/index.tsx
+++ example-web/pages/components/Signatures/index.tsx
@@ -3,16 +3,16 @@ import { useSignatures } from '@impact-market/utils/useSignatures';
 import WalletConnection from '../WalletConnection';
 
 const Signatures = () => {
-    const { signMessage, signTyped } = useSignatures();
+    const { signMessage, signTypedData } = useSignatures();
 
     const handleSignature = () => signMessage('hello');
 
-    const handleSignTyped = () => signTyped().then(console.log).catch(console.error);
+    const handleSignTypedData = () => signTypedData('Proving my identity').then(console.log).catch(console.error);
 
     return (
         <WalletConnection title="Signatures">
             <button onClick={handleSignature}>signMessage</button>
-            <button onClick={handleSignTyped}>signTyped</button>
+            <button onClick={handleSignTypedData}>signTypedData</button>
         </WalletConnection>
     );
 };
