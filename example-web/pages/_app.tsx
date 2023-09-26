a8f25119d56953a3cb2a963d5b7ce49a4d2294b4
Bernardo Vieira <bernardo@impactmarket.com>
2023-09-26 20:02:55 +0100

    draft web3modal v3

--- example-web/pages/_app.tsx
+++ example-web/pages/_app.tsx
@@ -1,24 +1,36 @@
 import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import { EthereumClient, w3mConnectors } from '@web3modal/ethereum';
-import { Web3Modal } from '@web3modal/react';
-import { celo } from '@wagmi/chains';
+import { WagmiConfig } from 'wagmi';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
+import { celoAlfajores } from '@wagmi/chains';
 
+const chains = [celoAlfajores];
 const projectId = 'e14be5c27cfd796596686bdc6876e836';
 
-const { chains, publicClient } = configureChains(
-    [celo],
-    [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default.http[0] }) })]
-);
 
-const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors: w3mConnectors({ projectId, chains }),
-    publicClient,
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+};
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({
+    wagmiConfig,
+    projectId,
+    chains,
+    featuredWalletIds: [
+        // libera
+        'b7cd38c9393f14b8031bc10bc0613895d0d092c33d836547faf8a9b782f6cbcc',
+        // valora
+        'd01c7758d741b363e637a817a09bcf579feae4db9f5bb16f599fdd1f66e2f974',
+        // metamask
+        'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96'
+    ],
+    themeMode: 'light',
+    defaultChain: celoAlfajores,
 });
-
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -26,18 +38,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             <WagmiConfig config={wagmiConfig}>
                 <Component {...pageProps} />
             </WagmiConfig>
-            <Web3Modal
-                projectId={projectId}
-                ethereumClient={ethereumClient}
-                explorerRecommendedWalletIds={[
-                    // libera
-                    'b7cd38c9393f14b8031bc10bc0613895d0d092c33d836547faf8a9b782f6cbcc',
-                    // valora
-                    'd01c7758d741b363e637a817a09bcf579feae4db9f5bb16f599fdd1f66e2f974',
-                    // metamask
-                    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96'
-                ]}
-            />
         </>
     );
 }
--- example-web/pages/components/WalletConnection.tsx
+++ example-web/pages/components/WalletConnection.tsx
@@ -1,10 +1,11 @@
-import { Web3Button } from '@web3modal/react'
 import React from 'react';
 import { useAccount } from 'wagmi';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 
 const WalletConnection = (props: { children: any; title?: string }) => {
     const { children, title } = props;
     const { address } = useAccount();
+    const { open } = useWeb3Modal();
 
     // if (!initialised) {
     //     return <div>loading...</div>;
@@ -14,7 +15,7 @@ const WalletConnection = (props: { children: any; title?: string }) => {
         <div>
             {!!title && <h2>{title}</h2>}
             <div style={{ marginBottom: 32, marginTop: title ? 8 : 0 }}>
-                <Web3Button />
+                <button onClick={() => open()}>Open Connect Modal</button>
             </div>
             {address && (
                 <>
