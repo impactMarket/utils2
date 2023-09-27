cbd15efaae2b588c3b414ac3da3e34a7fa02ed6e
Bernardo Vieira <bernardo@impactmarket.com>
2023-09-27 09:06:24 +0100

    remove unnecessary wallet

--- example-web/pages/_app.tsx
+++ example-web/pages/_app.tsx
@@ -1,17 +1,34 @@
 import type { AppProps } from 'next/app';
-import { WagmiConfig } from 'wagmi';
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
+import { createWeb3Modal } from '@web3modal/wagmi/react';
 import { celoAlfajores } from '@wagmi/chains';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { walletConnectProvider } from '@web3modal/wagmi';
 
-const chains = [celoAlfajores];
 const projectId = 'e14be5c27cfd796596686bdc6876e836';
 
-
 const metadata = {
-    name: 'impactMarket utils'
+    name: 'impactMarket utils',
+    description: 'impactMarket utils Example',
+    url: 'https://utils-flame.vercel.app',
+    icons: ['https://avatars.githubusercontent.com/u/42247406']
 };
 
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+const { chains, publicClient } = configureChains(
+    [celoAlfajores],
+    [walletConnectProvider({ projectId }), publicProvider()]
+);
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: [
+        new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+        new InjectedConnector({ chains, options: { shimDisconnect: true } })
+    ],
+    publicClient
+});
 
 createWeb3Modal({
     wagmiConfig,
@@ -24,7 +41,7 @@ createWeb3Modal({
         'd01c7758d741b363e637a817a09bcf579feae4db9f5bb16f599fdd1f66e2f974'
     ],
     themeMode: 'light',
-    defaultChain: celoAlfajores,
+    defaultChain: celoAlfajores
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
