c3b6d64493bf614ad2467511e157f42b0df6baec
Bernardo Vieira <bernardo@impactmarket.com>
2023-10-24 12:15:44 +0100

    bump deps + remove sentry from example

--- example-web/next.config.js
+++ example-web/next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const { withSentryConfig } = require('@sentry/nextjs');
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: false, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
 const webpack = (config, { webpack }) => {
     config.resolve.fallback = {
         ...config.resolve.fallback,
@@ -38,4 +24,4 @@ const nextConfig = {
     }
 };
 
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
+module.exports = nextConfig;
